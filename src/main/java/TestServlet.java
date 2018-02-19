75e766224dedc683899578634ecbb330ace4ae1c
Alisher Iskakov <finesome@gmail.com>
2018-02-20 01:26:18 +0600

    Added gson & junit as dependencies; changed index.jsp a bit; small change in TestServlet imports

--- src/main/java/TestServlet.java
+++ src/main/java/TestServlet.java
@@ -2,12 +2,8 @@ import org.apache.commons.text.WordUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-
+import javax.servlet.http.*;
+import java.io.*;
 
 @WebServlet(name = "TodoList", urlPatterns = {"/test"})
 public class TestServlet extends HttpServlet {
--- web/index.jsp
+++ web/index.jsp
@@ -11,6 +11,6 @@
     <title>$Title$</title>
   </head>
   <body>
-  $END$
+  <h1>Check check check, левое ухо, правое ухо</h1>
   </body>
 </html>
