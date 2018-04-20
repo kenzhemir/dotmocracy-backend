1e667236e9f4a2dcd1d2e232e074f50675ae3c8b
Aslan Assylkhanov <aslan.assylkhanov@nu.edu.kz>
2018-04-20 17:36:53 +0600

    Ideas entity corrected

--- src/main/java/models/IdeasEntity.java
+++ src/main/java/models/IdeasEntity.java
@@ -17,18 +17,21 @@ public class IdeasEntity {
     private String name;
     private Long boardsId;
     private String description;
+    private Long voteCount;
+    private Long voteTotal;
+
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "id")
-    public Long getId() {
+    public long getId() {
         return id;
     }
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "id")
-    public void setId(Long id) {
+    public void setId(long id) {
         this.id = id;
     }
 
@@ -62,6 +65,26 @@ public class IdeasEntity {
         this.description = description;
     }
 
+    @Basic
+    @Column(name = "vote_count")
+    public Long getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Long voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    @Basic
+    @Column(name = "vote_total")
+    public Long getVoteTotal() {
+        return voteTotal;
+    }
+
+    public void setVoteTotal(Long voteTotal) {
+        this.voteTotal = voteTotal;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -73,16 +96,21 @@ public class IdeasEntity {
         if (name != null ? !name.equals(that.name) : that.name != null) return false;
         if (boardsId != null ? !boardsId.equals(that.boardsId) : that.boardsId != null) return false;
         if (description != null ? !description.equals(that.description) : that.description != null) return false;
+        if (voteCount != null ? !voteCount.equals(that.voteCount) : that.voteCount != null) return false;
+        if (voteTotal != null ? !voteTotal.equals(that.voteTotal) : that.voteTotal != null) return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
+
         int result = (int) (id ^ (id >>> 32));
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (boardsId != null ? boardsId.hashCode() : 0);
         result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (voteCount != null ? voteCount.hashCode() : 0);
+        result = 31 * result + (voteTotal != null ? voteTotal.hashCode() : 0);
         return result;
     }
 }
